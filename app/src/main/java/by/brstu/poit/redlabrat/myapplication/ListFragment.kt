4f40d8621e3df27f2f093169d7a3b08be4078708
Alex Brych <redlabra@gmail.com>
2021-02-16 13:07:11 +0300

    Displaying list of planets

--- app/src/main/java/by/brstu/poit/redlabrat/myapplication/ListFragment.kt
+++ app/src/main/java/by/brstu/poit/redlabrat/myapplication/ListFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.brstu.poit.redlabrat.myapplication.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
+    companion object {
+        val listOfPlanets = listOf<String>("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    }
+
     private lateinit var binding: FragmentListBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +29,8 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.recyclerView.adapter
+        binding.recyclerView.adapter = PlanetListAdapter(listOfPlanets)
+//        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.loadingProgress.isVisible = false
     }
 }
