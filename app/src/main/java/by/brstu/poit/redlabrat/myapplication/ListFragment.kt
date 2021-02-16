03acbdeec574332184e1ac5a1d2b5ddcae296512
Alex Brych <redlabra@gmail.com>
2021-02-16 12:45:19 +0300

    Binding implemented for Fragment and Activity

--- app/src/main/java/by/brstu/poit/redlabrat/myapplication/ListFragment.kt
+++ app/src/main/java/by/brstu/poit/redlabrat/myapplication/ListFragment.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import by.brstu.poit.redlabrat.myapplication.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        binding = FragmentListBinding.inflate(inflater)
+        return binding.root
     }
 }
--- app/src/main/java/by/brstu/poit/redlabrat/myapplication/MainActivity.kt
+++ app/src/main/java/by/brstu/poit/redlabrat/myapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package by.brstu.poit.redlabrat.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.brstu.poit.redlabrat.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportFragmentManager.beginTransaction()
             .add(R.id.contentContainer, ListFragment())
